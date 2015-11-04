2c81054a5b4f9227c7abae908141f98a5c40b821
Aleynikov Timofey <aleynikov.timofey@gmail.ru>
2015-11-04 17:04:28 +0300

    Add means of setting field size.

--- Config.cs
+++ Config.cs
@@ -10,10 +10,13 @@ namespace ConsoleSimpleWorm
 {
     static class ConfigFile 
     {
-        public static void CreateConfigFile(char wormSymbol, char foodSymbol, int timeStamp)
+        public static void CreateConfigFile(int fieldWidth, int fieldHeight, char wormSymbol, char foodSymbol, int timeStamp)
         {
             XDocument doc = new XDocument(
                 new XElement("config",
+                    new XElement("field",
+                        new XAttribute("width", fieldWidth),
+                        new XAttribute("height", fieldHeight)),
                     new XElement("symbols",
                         new XElement("worm", wormSymbol),
                         new XElement("food", foodSymbol)),
@@ -23,18 +26,21 @@ namespace ConsoleSimpleWorm
 
         public static void CreateConfigFileDefault()
         {
-            CreateConfigFile('0', '*', 1000);
+            CreateConfigFile(100, 30, '0', '*', 1000);
         }
     }
     class Config
     {
         public readonly char _WORM_SYMBOL, _FOOD_SYMBOL;
-        public readonly int _TIME_STAMP;
+        public readonly int _FIELD_WIDTH, _FIELD_HEIGHT, _TIME_STAMP;
 
         public Config()
         {
             XElement conf = XDocument.Load("config.xml").Root;
+            XElement field = conf.Element("field");
             XElement syms = conf.Element("symbols");
+            _FIELD_WIDTH = Convert.ToInt32(field.Attribute("width").Value);
+            _FIELD_HEIGHT = Convert.ToInt32(field.Attribute("height").Value);
             _WORM_SYMBOL = syms.Element("worm").Value[0];
             _FOOD_SYMBOL = syms.Element("food").Value[0];
             _TIME_STAMP = Convert.ToInt32(conf.Element("timestamp").Value);
--- Program.cs
+++ Program.cs
@@ -22,6 +22,8 @@ namespace ConsoleSimpleWorm
                     ConfigFile.CreateConfigFileDefault();
                 }
             } while (conf == null);
+            Console.SetWindowSize(conf._FIELD_WIDTH, conf._FIELD_HEIGHT);
+            Console.SetBufferSize(conf._FIELD_WIDTH, conf._FIELD_HEIGHT);
             Console.WriteLine("Configs: {0} {1} {2}", 
                 conf._WORM_SYMBOL, 
                 conf._FOOD_SYMBOL, 
